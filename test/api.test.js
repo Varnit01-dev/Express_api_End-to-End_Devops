544860192476dafd4840345adbfd8338114c6626
Varnit Sharma <varnitsharma2015@gmail.com>
2025-04-10 00:27:58 +0530

    Add Ansible deployment & rollback playbooks, update CI/CD

--- test/api.test.js
+++ test/api.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../server');
+const app = require('../Server');
 
 describe('GET /', () => {
   it('should return hello message', async () => {
