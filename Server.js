8ca1065f28df90b4058bb3a7951be64b5d0a45e8
Varnit Sharma <varnitsharma2015@gmail.com>
2025-05-07 22:18:53 +0530

    Added Prometheus metrics

--- Server.js
+++ Server.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
+const prometheus = require('prom-client');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Create a Registry to register metrics
+const register = new prometheus.Registry();
+prometheus.collectDefaultMetrics({ register });
+
+// Add custom metrics
+const httpRequestDurationMicroseconds = new prometheus.Histogram({
+  name: 'http_request_duration_seconds',
+  help: 'Duration of HTTP requests in seconds',
+  labelNames: ['method', 'route', 'status_code'],
+  buckets: [0.1, 0.5, 1, 1.5, 2, 5]
+});
+register.registerMetric(httpRequestDurationMicroseconds);
+
+// Expose metrics endpoint
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', register.contentType);
+  res.end(await register.metrics());
+});
 
 const options = {
   definition: {
@@ -23,7 +43,6 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 app.get('/', (req, res) => {
   res.send('Hello, I am looking for Devops Opportunies!');
 });
-
 
 if (require.main === module) {
   const server = app.listen(PORT, () => {
